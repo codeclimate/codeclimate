03f72ea56e6e99e3d1b1759cb90a0b62d0a97566
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-06-02 15:20:21 -0400

    create validate-config command file to validate a user's codeclimate.yaml

---
+++ lib/cc/cli/validate-config.rb
@@ -0,0 +1,12 @@
+module CC
+  module CLI
+    class ValidateConfig < Command
+      
+      def run
+        #find .codeclimate.yml file
+        #check that it's a valid yml
+      end
+
+    end
+  end
+end
