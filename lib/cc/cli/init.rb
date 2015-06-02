e82d7a9585a7ad7c5dd44ea18ccb3672ef2d020d
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-06-02 17:58:28 -0400

    add template yaml file for copy and insertion to generate a new codeclimate_yaml for user

--- lib/cc/cli/init.rb
+++ lib/cc/cli/init.rb
@@ -5,13 +5,14 @@ module CC
     class Init < Command
       include CC::Analyzer
 
-      CODECLIMATE_YAML = ".codeclimate.yml".freeze
+      CODECLIMATE_YAML = ".testcodeclimate.yml".freeze
+      TEMPLATE_CODECLIMATE_YAML = "lib/cc/cli/template_codeclimate_config.yml"
       
       def run
         # print "Generating codeclimate.yaml config file if it doesn't exist"
         # self.show_processing
         # say "\nEdit and then try running 'validate-config' to check configuration."
-        say "Generating codeclimate.yaml config file if it doesn't exist.\nEdit and then try running 'validate-config' to check configuration."
+        say "Generating codeclimate.yml config file if it doesn't exist.\nEdit and then try running 'validate-config' to check configuration."
         create_codeclimate_yaml!
       end
 
@@ -23,7 +24,8 @@ module CC
         if filesystem.exist?(CODECLIMATE_YAML)
           return
         else
-          File.open(".codeclimate.yml", "w")
+          File.open(CODECLIMATE_YAML, "w")
+          IO.copy_stream(TEMPLATE_CODECLIMATE_YAML, CODECLIMATE_YAML)
         end
       end
 
