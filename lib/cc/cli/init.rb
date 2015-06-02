0c9553ab51004ffe0d7e66b4b44ed24ad5f454e6
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-06-02 15:19:46 -0400

    create init command file, with comment

---
+++ lib/cc/cli/init.rb
@@ -0,0 +1,12 @@
+module CC
+  module CLI
+    class Init < Command
+      
+      def run
+        # generate a code climate yaml
+        # tell user yml file was generated and prompt them to run 'validate-config' to lint, once done editing
+      end
+
+    end
+  end
+end
