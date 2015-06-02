27f07bcc755ba3f939a48c04aace87275e794ebb
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-06-02 17:30:36 -0400

    include filesystem class from cc/analyzer; add create codeclimate_yaml method

--- lib/cc/cli/init.rb
+++ lib/cc/cli/init.rb
@@ -1,10 +1,30 @@
+require "cc/analyzer"
+
 module CC
   module CLI
     class Init < Command
+      include CC::Analyzer
+
+      CODECLIMATE_YAML = ".codeclimate.yml".freeze
       
       def run
-        # generate a code climate yaml
-        # tell user yml file was generated and prompt them to run 'validate-config' to lint, once done editing
+        # print "Generating codeclimate.yaml config file if it doesn't exist"
+        # self.show_processing
+        # say "\nEdit and then try running 'validate-config' to check configuration."
+        say "Generating codeclimate.yaml config file if it doesn't exist.\nEdit and then try running 'validate-config' to check configuration."
+        create_codeclimate_yaml!
+      end
+
+      def filesystem 
+        @filesystem ||= Filesystem.new(".")
+      end
+
+      def create_codeclimate_yaml!
+        if filesystem.exist?(CODECLIMATE_YAML)
+          return
+        else
+          File.open(".codeclimate.yml", "w")
+        end
       end
 
     end
