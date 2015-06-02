9330739189678a6e560184a9eeb691e0ce122155
patrick brisbin <pbrisbin@gmail.com>
2015-06-02 14:10:21 -0400

    Add coverage on Filesystem

---
+++ spec/cc/analyzer/filesystem_spec.rb
@@ -0,0 +1,54 @@
+require "spec_helper"
+require "cc/analyzer"
+
+module CC::Analyzer
+  describe Filesystem do
+    describe "#exists?" do
+      it "returns true for files that exist" do
+        root = Dir.mktmpdir
+        File.write(File.join(root, "foo.rb"), "")
+
+        filesystem = Filesystem.new(root)
+
+        filesystem.exist?("foo.rb").must_equal(true)
+        filesystem.exist?("bar.rb").must_equal(false)
+      end
+    end
+
+    describe "#read_path" do
+      it "returns the content for the given file" do
+        root = Dir.mktmpdir
+        File.write(File.join(root, "foo.rb"), "Foo")
+        File.write(File.join(root, "bar.rb"), "Bar")
+
+        filesystem = Filesystem.new(root)
+
+        filesystem.read_path("foo.rb").must_equal("Foo")
+        filesystem.read_path("bar.rb").must_equal("Bar")
+      end
+    end
+
+    describe "#file_paths" do
+      it "returns all regular files in the root" do
+        root = Dir.mktmpdir
+        Dir.mkdir(File.join(root, "foo"))
+        Dir.mkdir(File.join(root, "bar"))
+        File.write(File.join(root, "foo.rb"), "")
+        File.write(File.join(root, "foo", "foo.rb"), "")
+        File.write(File.join(root, "foo", "bar.rb"), "")
+        File.write(File.join(root, "bar", "foo.rb"), "")
+        File.write(File.join(root, "bar", "bar.rb"), "")
+
+        filesystem = Filesystem.new(root)
+
+        filesystem.file_paths.sort.must_equal([
+          "foo.rb",
+          "foo/foo.rb",
+          "foo/bar.rb",
+          "bar/foo.rb",
+          "bar/bar.rb",
+        ].sort)
+      end
+    end
+  end
+end
