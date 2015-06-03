cfc89541be178d4bd7ac8d73b9ad246f4e4caff3
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-06-03 15:18:28 -0400

    change 'context's to 'describe's to avoid error of undefined method 'context' when running tests- change from 'expect(x).to eq(y)' to 'x.must_equal(y)'

--- spec/cc/cli/init_spec.rb
+++ spec/cc/cli/init_spec.rb
@@ -4,35 +4,35 @@ require "cc/cli"
 module CC::CLI
   describe Init do
     describe "#run" do
-      context "when no .codeclimate.yml file is present in working directory" do
+      describe "when no .codeclimate.yml file is present in working directory" do
         it "creates a correct .codeclimate.yml file" do
           temp = Dir.mktmpdir
           
           Dir.chdir(temp) do
             filesystem = CC::Analyzer::Filesystem.new(".")
             
-            expect(filesystem.exist?(".codeclimate.yml")).to eq(false)
+            filesystem.exist?(".codeclimate.yml").must_equal(false)
 
             init = Init.new
             init.run
 
-            expect(filesystem.exist?(".codeclimate.yml")).to eq(true)
+            filesystem.exist?(".codeclimate.yml").must_equal(true)
 
             new_content = File.read(".codeclimate.yml")
 
-            expect(new_content).to eq(Init::TEMPLATE_CODECLIMATE_YAML)
+            new_content.must_equal(Init::TEMPLATE_CODECLIMATE_YAML)
           end
         end 
       end
 
-      context "when a .codeclimate.yml file is already present in working directory" do
+      describe "when a .codeclimate.yml file is already present in working directory" do
         it "does not create a new file or overwrite the old" do
           temp = Dir.mktmpdir
 
           Dir.chdir(temp) do
             filesystem = CC::Analyzer::Filesystem.new(".")
 
-            expect(filesystem.exist?(".codeclimate.yml")).to eq(false)
+            filesystem.exist?(".codeclimate.yml").must_equal(false)
 
 
             yaml_content_before = "This is a test yaml!"
@@ -40,16 +40,16 @@ module CC::CLI
               f.write(yaml_content_before)
             end
 
-            expect(filesystem.exist?(".codeclimate.yml")).to eq(true)
+            filesystem.exist?(".codeclimate.yml").must_equal(true)
 
             init = Init.new
             init.run
 
-            expect(filesystem.exist?(".codeclimate.yml")).to eq(true)
+            filesystem.exist?(".codeclimate.yml").must_equal(true)
 
             content_after = File.read(".codeclimate.yml")
 
-            expect(content_after).to eq(yaml_content_before)
+            content_after.must_equal(yaml_content_before)
           end
         end
       end
