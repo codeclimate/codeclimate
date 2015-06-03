efb049c54d0ff40b2181580af612326f2c1da2a3
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-06-03 14:05:03 -0400

    remove conditional from create_codeclimate_yaml method, as already present in #run

--- lib/cc/cli/init.rb
+++ lib/cc/cli/init.rb
@@ -6,26 +6,48 @@ module CC
       include CC::Analyzer
 
       CODECLIMATE_YAML = ".codeclimate.yml".freeze
-      TEMPLATE_CODECLIMATE_YAML = "lib/cc/cli/template_codeclimate_config.yml"
+      TEMPLATE_CODECLIMATE_YAML = <<-YAML
+#
+# ---Choose Your Languages---
+# To disable analysis for a certain language, set the language to `false`.
+# For help setting your languages:
+# http://docs.codeclimate.com/article/169-configuring-analysis-languages
+#
+languages:
+   Ruby: true
+   JavaScript: true
+   Python: true
+   PHP: true
+#
+# ---Exclude Files or Directories---
+# List the files or directories you would like excluded from our analysis.
+# For help setting your exclude paths:
+# http://docs.codeclimate.com/article/166-excluding-files-folders
+#
+exclude_paths:
+ - "test/*"
+      YAML
       
       def run
-        say "Generating .codeclimate.yml config file if it doesn't exist.\nEdit and then try running 'validate-config' to check configuration."
-        create_codeclimate_yaml!
+        if filesystem.exist?(CODECLIMATE_YAML)
+          say "Config file .codeclimate.yml already present.\nTry running 'validate_config' to check configuration." 
+        else
+          create_codeclimate_yaml!
+          say "Config file .codeclimate.yml successfully generated.\nEdit and then try running 'validate_config' to check configuration."
+        end
       end
+
+      private
 
       def filesystem 
         @filesystem ||= Filesystem.new(".")
       end
 
       def create_codeclimate_yaml!
-        if filesystem.exist?(CODECLIMATE_YAML)
-          return
-        else
-          File.open(CODECLIMATE_YAML, "w")
-          IO.copy_stream(TEMPLATE_CODECLIMATE_YAML, CODECLIMATE_YAML)
+        File.open(CODECLIMATE_YAML, "w") do |f|
+          f.write(TEMPLATE_CODECLIMATE_YAML)
         end
       end
-
     end
   end
 end
