49389bcf103b7d33a4d5a52138a997d9048c7f35
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-06-09 17:00:17 -0400

    remove redundant to_yaml method - already available in library. create contract_references method to return hash to format listing only path to config file (not config file contents) in engines

--- lib/cc/analyzer/config.rb
+++ lib/cc/analyzer/config.rb
@@ -38,44 +38,9 @@ module CC
         end
       end
 
-      def attribute_to_s(attribute, buffer="")
-        string = ""
-        if !attribute.instance_of?(Hash)
-          string += " #{attribute}\n"
-        else
-          attribute.each do |key, value|
-            string += "\n#{buffer} #{key}:"
-            string += attribute_to_s(value, buffer + BUFFER)
-          end
-        end
-        string
-      end
-
       def to_yaml
-        yaml = ""
-        @config.each do |key, value|
-          if key == "engines"
-            yaml += engines_to_yaml
-          else
-            yaml += "#{key}:"
-            yaml += attribute_to_s(value)
-          end
-        end
-        yaml
-      end
-
-      def engines_to_yaml
-        yaml = "engines:\n"
-        @config["engines"].each do |name, engine_config|
-          yaml += BUFFER + "#{name}:\n"
-          engine_config.each do |key, value|
-            if key == "config_file"
-              value = value["path"]
-            end
-            yaml += BUFFER*2 + "#{key}: #{value}\n"
-          end
-        end
-        yaml
+        contract_references
+        @config.to_yaml
       end
 
     private
@@ -97,6 +62,15 @@ module CC
           end
         end
       end
+
+      def contract_references
+        @config["engines"].each do |name, engine_config|
+          if engine_config["config_file"]
+            @config["engines"][name]["config_file"] = engine_config["config_file"]["path"]
+          end
+        end
+      end
+
     end
   end
 end
