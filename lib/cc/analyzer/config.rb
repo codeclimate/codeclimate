43b82e4bd666ad9b76fe25dbff543afe2cdad2f8
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-06-09 14:14:19 -0400

    add methods for writing config hash to yaml file

--- lib/cc/analyzer/config.rb
+++ lib/cc/analyzer/config.rb
@@ -4,6 +4,8 @@ module CC
   module Analyzer
     class Config
 
+      BUFFER = "  ".freeze
+
       def initialize(config_body)
         @config = YAML.safe_load(config_body)
 
@@ -17,6 +19,55 @@ module CC
 
       def engine_names
         @config["engines"].keys
+      end
+
+      def engine_enabled?(engine_name)
+        engine = @config["engines"][engine_name]
+        engine && engine["enabled"]
+      end
+
+      def enable_engine(engine_name)
+        @config["engines"][engine_name] = { "enabled" => true }
+      end
+
+      def attribute_to_s(attribute, buffer="")
+        string = ""
+        if !attribute.instance_of?(Hash)
+          string += " #{attribute}\n"
+        else
+          attribute.each do |key, value|
+            string += "\n#{buffer} #{key}:"
+            string += attribute_to_s(value, buffer + BUFFER)
+          end
+        end
+        string
+      end
+
+      def to_yaml
+        yaml = ""
+        @config.each do |key, value|
+          if key == "engines"
+            yaml += engines_to_yaml
+          else
+            yaml += "#{key}:"
+            yaml += attribute_to_s(value)
+          end
+        end
+        yaml
+      end
+
+      def engines_to_yaml
+        yaml = "engines:\n"
+        @config["engines"].each do |name, engine_config|
+          yaml += BUFFER + "#{name}:\n"
+          engine_config.each do |key, value|
+            if key == "config_file"
+              value = value["path"]
+            end
+            yaml += BUFFER*2 + "#{key}: #{value}\n"
+          end
+        end
+        yaml
       end
 
     private
@@ -33,12 +84,11 @@ module CC
         @config["engines"].each do |name, engine_config|
           if (path = engine_config["config_file"])
             if File.exist?(path)
-              engine_config["config_file"] = File.read(path)
+              engine_config["config_file"] = { "path" => "#{path}", "content" => File.read(path) }
             end
           end
         end
       end
-
     end
   end
 end
