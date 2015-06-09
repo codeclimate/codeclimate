70c8bd1d81cee0dad8ce5f3770e716a4cb033c79
GordonDiggs <gordon@gordondiggs.com>
2015-06-09 10:40:15 -0400

    Add test for command_name

---
+++ spec/cc/cli/runner_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+module CC::CLI
+  describe Runner do
+    describe "#command_name" do
+      it "parses subclasses" do
+        Runner.new(['analyze:this']).command_name.must_equal("Analyze::This")
+      end
+
+      it "returns class names" do
+        Runner.new(['analyze']).command_name.must_equal("Analyze")
+      end
+    end
+  end
+end
