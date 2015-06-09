6046012dc3742a55de882c7d2f70ba0d511ef5df
GordonDiggs <gordon@gordondiggs.com>
2015-06-09 10:54:53 -0400

    Fix quotes

--- spec/cc/cli/runner_spec.rb
+++ spec/cc/cli/runner_spec.rb
@@ -1,14 +1,14 @@
-require 'spec_helper'
+require "spec_helper"
 
 module CC::CLI
   describe Runner do
     describe "#command_name" do
       it "parses subclasses" do
-        Runner.new(['analyze:this']).command_name.must_equal("Analyze::This")
+        Runner.new(["analyze:this"]).command_name.must_equal("Analyze::This")
       end
 
       it "returns class names" do
-        Runner.new(['analyze']).command_name.must_equal("Analyze")
+        Runner.new(["analyze"]).command_name.must_equal("Analyze")
       end
     end
   end
