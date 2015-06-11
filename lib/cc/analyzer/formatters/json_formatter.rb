bb5b91a9641b9e02728110c22e752c5565a7048f
GordonDiggs <gordon@gordondiggs.com>
2015-06-11 12:58:11 -0400

    Exit in json formatter if the analysis fails

--- lib/cc/analyzer/formatters/json_formatter.rb
+++ lib/cc/analyzer/formatters/json_formatter.rb
@@ -5,6 +5,12 @@ module CC
         def write(data)
           puts JSON.parse(data).to_json if data.present?
         end
+
+        def failed(output)
+          puts "\nAnalysis failed with the following output:"
+          puts output
+          exit 1
+        end
       end
     end
   end
