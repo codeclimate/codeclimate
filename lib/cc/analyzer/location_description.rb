c87b92ec2d0455bcf9aaaf2e33a51034760e37c5
GordonDiggs <gordon@gordondiggs.com>
2015-06-15 13:42:02 -0400

    Restructure class and add tests

--- lib/cc/analyzer/location_description.rb
+++ lib/cc/analyzer/location_description.rb
@@ -8,31 +8,13 @@ module CC
       def to_s
         str = ""
         if location["lines"]
-          str << location["lines"]["begin"].to_s
-
-          if location["lines"]["end"]
-            str << "-#{location["lines"]["end"]}"
-          end
+          str << render_lines
         elsif positions = location["positions"]
-          if positions["begin"]["line"]
-            str << positions["begin"]["line"].to_s
-            if positions["begin"]["column"]
-              str << ":#{positions["begin"]["column"]}"
-            end
-          elsif positions["begin"]["offset"]
-            str << positions["begin"]["offset"].to_s
-          end
+          str << render_position(positions["begin"])
 
-          if ending = positions["end"]
+          if positions["end"]
             str << "-"
-            if ending["line"]
-              str << ending["line"].to_s
-              if ending["column"]
-                str << ":#{ending["column"]}"
-              end
-            elsif ending["offset"]
-              str << ending["offset"].to_s
-            end
+            str << render_position(positions["end"])
           end
         end
         str
@@ -41,6 +23,25 @@ module CC
       private
 
       attr_reader :location
+
+      def render_lines
+        str = location["lines"]["begin"].to_s
+        str << "-#{location["lines"]["end"]}" if location["lines"]["end"]
+        str
+      end
+
+      def render_position(position)
+        str = ""
+
+        if position["line"]
+          str << position["line"].to_s
+          str << ":#{position["column"]}" if position["column"]
+        elsif position["offset"]
+          str << position["offset"].to_s
+        end
+
+        str
+      end
     end
   end
 end
