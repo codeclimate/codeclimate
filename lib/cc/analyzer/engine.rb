08e4498eb7e77c2054c688c3e6d7320ce668138c
GordonDiggs <gordon@gordondiggs.com>
2015-06-26 10:53:47 -0400

    Use UID and GID

--- lib/cc/analyzer/engine.rb
+++ lib/cc/analyzer/engine.rb
@@ -86,7 +86,7 @@ module CC
           "--net", "none",
           "--volume", "#{@code_path}:/code:ro",
           "--env-file", env_file,
-          "--user", "app",
+          "--user", "9000:9000",
           @metadata["image_name"],
           @metadata["command"], # String or Array
         ].flatten.compact
