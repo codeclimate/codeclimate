5d90ce4389a81592adda099f984c1fd0bd4505d4
patrick brisbin <pbrisbin@gmail.com>
2015-07-23 11:04:27 -0400

    Support dev_mode directly in EngineRegistry

--- lib/cc/analyzer/engine_registry.rb
+++ lib/cc/analyzer/engine_registry.rb
@@ -3,17 +3,28 @@ require "safe_yaml"
 module CC
   module Analyzer
     class EngineRegistry
-      def initialize
+      def initialize(dev_mode = false)
         @path = File.expand_path("../../../../config/engines.yml", __FILE__)
         @config = YAML.safe_load_file(@path)
+        @dev_mode = dev_mode
       end
 
       def [](engine_name)
-        @config[engine_name]
+        if dev_mode?
+          { "image"=>"codeclimate/codeclimate-#{engine_name}:latest" }
+        else
+          @config[engine_name]
+        end
       end
 
       def list
         @config
+      end
+
+      private
+
+      def dev_mode?
+        @dev_mode
       end
     end
   end
