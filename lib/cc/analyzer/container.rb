81fd528dd70cee9e1d973edb2e69d56829cf0720
patrick brisbin <pbrisbin@gmail.com>
2015-08-14 11:32:11 -0400

    WIP: Sketch for improving Engine
    New responsibility breakdown:
    
    **Container**: launch a docker container, give its stdout to the caller,
    capture its stderr, monitor its process, handle timeouts, call
    life-cycle events on the container-log given.
    
    **Engine::ContainerLog**: perform the statsd calls on container
    life-cycle events and then forward those events to the log its been
    given; sort of a ContainerLog middleware stack idea.
    
    **Engine**: launch and run a Container for the engine, specify how to
    shuttle null-delimited stdout to the formatter, compose its
    container-log with the one given.
    
    Benefits:
    
    - Engine is smaller
    - Engine#run is smaller
    - Container is singly-responsible and reusable for Clone in builder
    - Easier to reason about (complex timeout logic is clearer IMO)
    
    Notable change:
    
    Rather than spawning `docker kill` I'm using Ruby's `Process.kill`, the
    two should be equivalent and this was more convenient with the way the
    code is now (no need to track container-name).

---
+++ lib/cc/analyzer/container.rb
@@ -0,0 +1,70 @@
+module CC
+  module Analyzer
+    class Container
+      TIMEOUT = 15 * 60 # 15m
+
+      def initialize(image, command = nil, log = nil)
+        @image = image
+        @command = command
+        @log = log || NullContainerLog.new
+
+        @output_delimeter = "\n"
+        @on_output = ->(*)
+
+        @timed_out = false
+        @stderr_io = StringIO.new
+      end
+
+      def on_output(delimeter = "\n", &block)
+        @output_delimeter = delimeter
+        @on_output = block
+      end
+
+      def run(options)
+        @log.started(@image)
+
+        pid, _, out, err = POSIX::Spawn.popen4(*docker_run_command)
+
+        t_out = read_stdout(out)
+        t_err = read_stderr(err)
+        t_timeout = timeout_thread(pid)
+
+        _, status = Process.waitpid2(pid)
+
+        t_timeout.kill
+      ensure
+        t_timeout.kill if t_timeout
+
+        if @timed_out
+          t_out.kill if t_out
+          t_err.kill if t_err
+        else
+          t_out.join if t_out
+          t_err.join if t_err
+        end
+      end
+
+      private
+
+      def read_stdout(out)
+        Thread.new do
+          out.each_line(@output_delimeter, &@on_output)
+        end
+      end
+
+      def read_stderr(err)
+        Thread.new do
+          err.each_line { |line| stderr_io.write(line) }
+        end
+      end
+
+      def timeout_thread(pid)
+        Thread.new do
+          sleep TIMEOUT
+          Process.kill("KILL", pid)
+          @timed_out = true
+        end
+      end
+    end
+  end
+end
