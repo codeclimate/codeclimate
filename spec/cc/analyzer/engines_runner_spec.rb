3fca04d7c8151bbc2b0c18300c17a5d4dd793e60
Francis Hwang <sera@fhwang.net>
2015-10-15 16:00:09 -0400

    Move relevant specs to engines_spec.rb.

--- spec/cc/analyzer/engines_runner_spec.rb
+++ spec/cc/analyzer/engines_runner_spec.rb
@@ -24,87 +24,11 @@ module CC::Analyzer
       EnginesRunner.new(registry, formatter, "/code", config).run
     end
 
-    it "does not raise for invalid engine names" do
-      config = config_with_engine("an_engine")
-      runner = EnginesRunner.new({}, null_formatter, "/code", config)
-
-      lambda { runner.run }
-    end
-
     it "raises for no enabled engines" do
       config = stub(engines: {})
       runner = EnginesRunner.new({}, null_formatter, "/code", config)
 
       lambda { runner.run }.must_raise(EnginesRunner::NoEnabledEngines)
-    end
-
-    it "massages data for the config" do
-      config = CC::Yaml.parse <<-EOYAML
-        engines:
-          rubocop:
-            enabled: true
-            config:
-              file: rubocop.yml
-      EOYAML
-      registry = registry_with_engine("rubocop")
-      formatter = null_formatter
-
-      FileUtils.stubs(:readable_by_all?).at_least_once.returns(true)
-      expected_config = {
-        "enabled" => true,
-        "config" => "rubocop.yml",
-        :exclude_paths => [],
-        :include_paths => ["./"]
-      }
-
-      expect_engine_run("rubocop", "/code", formatter, expected_config)
-
-      EnginesRunner.new(registry, formatter, "/code", config).run
-    end
-
-    it "respects .gitignore paths" do
-      make_file(".ignorethis")
-      make_file(".gitignore", ".ignorethis\n")
-      config = CC::Yaml.parse <<-EOYAML
-        engines:
-          rubocop:
-            enabled: true
-      EOYAML
-      registry = registry_with_engine("rubocop")
-      formatter = null_formatter
-
-      FileUtils.stubs(:readable_by_all?).at_least_once.returns(true)
-      expected_config = {
-        "enabled" => true,
-        :exclude_paths => %w[.ignorethis],
-        :include_paths => %w[.gitignore]
-      }
-
-      expect_engine_run("rubocop", "/code", formatter, expected_config)
-
-      EnginesRunner.new(registry, formatter, "/code", config).run
-    end
-
-    describe "when the source directory contains all readable files, and there are no ignored files" do
-      let(:engines_config) { config_with_engine("an_engine") }
-      let(:formatter) { null_formatter }
-      let(:registry) { registry_with_engine("an_engine") }
-
-      before do
-        make_file("root_file.rb")
-        make_file("subdir/subdir_file.rb")
-      end
-
-      it "gets include_paths from IncludePathBuilder" do
-        IncludePathsBuilder.expects(:new).with([], []).returns(mock(build: ['.']))
-        expected_config = {
-          "enabled" => true,
-          :exclude_paths => [],
-          :include_paths => ['.']
-        }
-        expect_engine_run("an_engine", "/code", formatter, expected_config)
-        EnginesRunner.new(registry, formatter, "/code", engines_config).run
-      end
     end
 
     describe "when the formatter does not respond to #close" do
