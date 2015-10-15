a98c93d1172619ac01628a6f7a3792b3468ad6bf
Francis Hwang <sera@fhwang.net>
2015-10-15 15:39:19 -0400

    Extract Analyzer::Engines from Analyzer::EnginesRunner.
    This reduces the responsibility of Analyzer::EnginesRunner so it only
    runs analyses on an array of pre-instantiated Engines. Choosing and
    instantiating engines with the correct config is now the responsibility
    of Analyzer::Engines.
    
    This is motivated in part by desired changes in stream handling in
    platform -- separating these responsibilities here will give us more
    flexibility elsewhere. In addition, the hope is that this might lead to
    phasing out EnginesRunner entirely.

--- lib/cc/analyzer/engines_runner.rb
+++ lib/cc/analyzer/engines_runner.rb
@@ -13,14 +13,21 @@ module CC
         @config = config
         @requested_paths = requested_paths
         @container_label = container_label
+        @engines = Engines.new(
+          registry: registry,
+          config: config,
+          container_label: container_label,
+          source_dir: source_dir,
+          requested_paths: requested_paths
+        )
       end
 
       def run(container_listener = ContainerListener.new)
-        raise NoEnabledEngines if engines.empty?
+        raise NoEnabledEngines if @engines.empty?
 
         @formatter.started
 
-        engines.each { |engine| run_engine(engine, container_listener) }
+        @engines.each { |engine| run_engine(engine, container_listener) }
 
         @formatter.finished
       ensure
@@ -34,60 +41,6 @@ module CC
       def run_engine(engine, container_listener)
         @formatter.engine_running(engine) do
           engine.run(@formatter, container_listener)
-        end
-      end
-
-      def engines
-        @engines ||= enabled_engines.map do |name, config|
-          label = @container_label || SecureRandom.uuid
-
-          Engine.new(name, metadata(name), @source_dir, engine_config(config), label)
-        end
-      end
-
-      def engine_config(config)
-        config = config.merge(
-          exclude_paths: exclude_paths,
-          include_paths: include_paths
-        )
-
-        # The yaml gem turns a config file string into a hash, but engines expect the string
-        # So we (for now) need to turn it into a string in that one scenario.
-        # TODO: update the engines to expect the hash and then remove this.
-        if config.fetch("config", {}).keys.size == 1 && config["config"].key?("file")
-          config["config"] = config["config"]["file"]
-        end
-
-        config
-      end
-
-      def enabled_engines
-        {}.tap do |ret|
-          @config.engines.each do |name, config|
-            if config.enabled? && @registry.key?(name)
-              ret[name] = config
-            end
-          end
-        end
-      end
-
-      def metadata(engine_name)
-        @registry[engine_name]
-      end
-
-      def include_paths
-        IncludePathsBuilder.new(@config.exclude_paths || [], requested_paths).build
-      end
-
-      def exclude_paths
-        PathPatterns.new(@config.exclude_paths || []).expanded + gitignore_paths
-      end
-
-      def gitignore_paths
-        if File.exist?(".gitignore")
-          `git ls-files --others -i -z --exclude-from .gitignore`.split("\0")
-        else
-          []
         end
       end
     end
