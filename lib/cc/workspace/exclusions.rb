39ed353780d96364391247a42610f6527f7c9ec9
Will Fleming <will@flemi.ng>
2016-01-18 18:34:12 -0500

    Workspace: use the PathTree
    This changes Workspace to make use of PathTree for including & excluding
    paths.
    
    The important functional change is that glob patterns do not result in a
    warning: they are applied correctly.

--- lib/cc/workspace/exclusions.rb
+++ lib/cc/workspace/exclusions.rb
@@ -7,40 +7,18 @@ module CC
         @exclude_paths = exclude_paths.map { |p| normalize(p) }.compact
       end
 
-      def include?(path)
-        exclude_paths.include?(path)
+      def glob?(exclusion)
+        exclusion.include?("*")
       end
 
-      def apply?(path)
-        exclude_paths.any? do |exclude_path|
-          exclude_path.start_with?(path)
-        end
+      def expanded_glob(exclusion)
+        Dir.glob(exclusion)
       end
 
       private
 
       def normalize(pattern)
-        normalized = pattern.to_s.sub(%r{/\*\*(/\*)?$}, "")
-
-        if normalized.include?("*")
-          $stderr.puts(invalid_exclude_path_warning(pattern))
-        else
-          normalized
-        end
-      end
-
-      def invalid_exclude_path_warning(pattern)
-        <<-EOM
-WARNING: invalid exclude path: #{pattern.inspect}.
-
-Unfortunately, we no longer support wildcard patterns in exclude_paths, only
-file paths or directories relative to the project root. This pattern will be
-ignored.
-
-If you feel particularly limited by this, please open an Issue at
-https://github.com/codeclimate/codeclimate and we'll try to find a
-non-exclude_paths-based solution.
-        EOM
+        pattern.to_s.sub(%r{/\*\*(/\*)?$}, "")
       end
     end
   end
