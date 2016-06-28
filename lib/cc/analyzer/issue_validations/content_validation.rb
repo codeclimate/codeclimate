151fd43c22bc99fe834e1150d55d0238fdaa7959
Will Fleming <will@flemi.ng>
2016-06-28 13:52:08 -0400

    Validate Issue Content key (#465)
    We occassionally see invalid contents emited that will blow things up
    further down the line (like a `null` body, for example). This adds a
    validation to catch these things earlier.

---
+++ lib/cc/analyzer/issue_validations/content_validation.rb
@@ -0,0 +1,25 @@
+module CC
+  module Analyzer
+    module IssueValidations
+      class ContentValidation < Validation
+        def valid?
+          !has_content? || (content.is_a?(Hash) && content["body"].is_a?(String))
+        end
+
+        def message
+          "Content must be a hash containing a 'body' key with string contents"
+        end
+
+        private
+
+        def has_content?
+          object.key?("content")
+        end
+
+        def content
+          object["content"]
+        end
+      end
+    end
+  end
+end
