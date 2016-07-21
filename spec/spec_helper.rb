dad546754d5b72ab1e606d594cd97cb97eeccabd
Thanatas Pongpanotkorn <xaxadmin@users.noreply.github.com>
2016-07-22 01:52:12 +0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,2 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
-
-require "cc/cli"
-require "cc/yaml"
-
-Dir.glob("spec/support/**/*.rb").each(&method(:load))
-
-ENV.delete("CODECLIMATE_DOCKER")
-ENV["CODECLIMATE_TMP"] = Dir.mktmpdir
