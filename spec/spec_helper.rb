e7087f31229207b5bfd2042ac9d7956c3ae35b3b
Ale Paredes <alexandra@codeclimate.com>
2017-01-11 16:00:59 -0500

    Test branch (#542)
    * Add solano CI
    
    * Add codeclimate token

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
-require "simplecov"
-SimpleCov.start do
-  add_filter "/spec/"
-end
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+# require "simplecov"
+# SimpleCov.start do
+#   add_filter "/spec/"
+# end
 
 require "cc/cli"
 require "cc/yaml"
