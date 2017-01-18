a0f4991290d281a4d78f4a88a596eb1ae2fe5a8a
elcuervo <yo@brunoaguirre.com>
2017-01-17 22:43:22 -0300

    Basic call home

--- spec/cc/cli/runner_spec.rb
+++ spec/cc/cli/runner_spec.rb
@@ -2,6 +2,12 @@ require "spec_helper"
 
 module CC::CLI
   describe Runner do
+    before do
+      versions_resp = { version: "0.4.1" }
+      resp = double(code: "200", body: versions_resp.to_json)
+      stub_resp("versions.codeclimate.com", "255.255.255.255", resp)
+    end
+
     describe ".run" do
       it "rescues exceptions and prints a friendlier message" do
         Explode = Class.new(Command) do
