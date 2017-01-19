9176d87ce172ebd3e3618b5fcad06863be1c9b7c
Alexander Mankuta <alex@pointless.one>
2017-01-19 14:02:45 +0200

    add spec for other_locations in HTML formatter

--- spec/cc/analyzer/formatters/html_formatter_spec.rb
+++ spec/cc/analyzer/formatters/html_formatter_spec.rb
@@ -15,9 +15,9 @@ module CC
         end
 
         describe "#finished" do
-          it "outputs an HTML report" do
-            engine = double(name: "cool_engine")
+          let(:engine) { double(name: "cool_engine") }
 
+          it "outputs an HTML report" do
             stdout, = capture_io do
               issue = sample_issue(
                 "content" => {
@@ -30,6 +30,32 @@ module CC
 
             expect(stdout).to include("lib/cc/analyzer/config.rb")
             expect(stdout).to include("<h3>Sample Issue</h3>")
+            expect(stdout).to include("cool_engine")
+          end
+
+          it "outputs an HTML report with other_locations" do
+            stdout, = capture_io do
+              issue = sample_issue(
+                "other_locations" => [{
+                  "path" => "lib/cc/analyzer/engine.rb",
+                  "positions" => {
+                    "begin" => {
+                      "line" => 1,
+                      "column" => 2,
+                    },
+                    "end" => {
+                      "line" => 3,
+                      "column" => 4,
+                    },
+                  },
+                }],
+              )
+              write_from_engine(formatter, engine, issue)
+              formatter.finished
+            end
+
+            expect(stdout).to include("<summary>Other instances</summary>")
+            expect(stdout).to include("lib/cc/analyzer/engine.rb")
           end
         end
 
