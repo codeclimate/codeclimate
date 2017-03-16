54848ca7e1b12e8bb0da2f8ee5122cc6ba5f5bb5
Max Jacobson <max@hardscrabble.net>
2017-03-16 16:53:01 -0400

    fix double leaking across spec examples
    The problem is that we were "replacing resolvers" with an array of
    resolvers, which includes a double, and that array is persisted across
    spec examples, and doubles aren't allowed to outlive a spec. This is the
    only way I've found to clear that out.

--- spec/support/resolv_helpers.rb
+++ spec/support/resolv_helpers.rb
@@ -1,6 +1,6 @@
 module ResolvHelpers
   def stub_resolv(name, address)
-    dns = double
+    dns = double(:dns)
     allow(::Resolv::DNS).to receive(:new).and_return(dns)
     allow(dns).to receive(:each_address).
       with(name).and_yield(Resolv::IPv4.create(address))
@@ -14,4 +14,8 @@ end
 
 RSpec.configure do |conf|
   conf.include(ResolvHelpers)
+
+  conf.after do
+    ::Resolv::DefaultResolver.replace_resolvers([])
+  end
 end
