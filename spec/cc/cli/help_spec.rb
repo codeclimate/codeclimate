bedc95a35783b551e02393ef7e810e208dc0750c
Max Jacobson <max@hardscrabble.net>
2017-03-16 15:18:22 -0400

    Add nil check
    I hit a NoMethodError while running what turns out to be a nonsense
    command: `codeclimate help --no-check-version`

---
+++ spec/cc/cli/help_spec.rb
@@ -0,0 +1,50 @@
+require "spec_helper"
+
+module CC::CLI
+  describe Help do
+    describe "#run" do
+      it "gives general help when no args" do
+        stdout, stderr, exit_code = capture_io_and_exit_code do
+          Help.new.run
+        end
+
+        expect(exit_code).to eq(0)
+        expect(stderr).to be_blank
+        expect(stdout).to include("Available commands")
+      end
+
+      it "gives specific help for a provided command" do
+        stdout, stderr, exit_code = capture_io_and_exit_code do
+          Help.new(["analyze"]).run
+        end
+
+        expect(exit_code).to eq(0)
+        expect(stderr).to be_blank
+        expect(stdout).to include("Run analysis")
+      end
+
+      it "gives specific help for multiple provided commands" do
+        stdout, stderr, exit_code = capture_io_and_exit_code do
+          Help.new(["analyze", "init"]).run
+        end
+
+        expect(exit_code).to eq(0)
+        expect(stderr).to be_blank
+        expect(stdout).to include("Run analysis")
+        expect(stdout).to include("Generate a configuration")
+      end
+
+      it "warns on unknown commands" do
+        stdout, stderr, exit_code = capture_io_and_exit_code do
+          Help.new(["analyze", "init", "bogus"]).run
+        end
+
+        expect(exit_code).to eq(0)
+        expect(stderr).to be_blank
+        expect(stdout).to include("Run analysis")
+        expect(stdout).to include("Generate a configuration")
+        expect(stdout).to include("Unknown command: bogus")
+      end
+    end
+  end
+end
