bf8b36d8eeb5e123ebb14dd87a0984a990cbd0c7
patrick brisbin <pbrisbin@gmail.com>
2017-04-12 17:29:01 -0400

    Remove init and configuration inference
    We will be completely rewriting this sub-system for Quality Model.

--- spec/cc/cli/help_spec.rb
+++ spec/cc/cli/help_spec.rb
@@ -25,24 +25,22 @@ module CC::CLI
 
       it "gives specific help for multiple provided commands" do
         stdout, stderr, exit_code = capture_io_and_exit_code do
-          Help.new(["analyze", "init"]).run
+          Help.new(["analyze"]).run
         end
 
         expect(exit_code).to eq(0)
         expect(stderr).to be_blank
         expect(stdout).to include("Run analysis")
-        expect(stdout).to include("Generate a configuration")
       end
 
       it "warns on unknown commands" do
         stdout, stderr, exit_code = capture_io_and_exit_code do
-          Help.new(["analyze", "init", "bogus"]).run
+          Help.new(["analyze", "bogus"]).run
         end
 
         expect(exit_code).to eq(0)
         expect(stderr).to be_blank
         expect(stdout).to include("Run analysis")
-        expect(stdout).to include("Generate a configuration")
         expect(stdout).to include("Unknown command: bogus")
       end
     end
