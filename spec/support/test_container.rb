afb49287a2f196fd4140fc7b15ffb341c52c5989
patrick brisbin <pbrisbin@gmail.com>
2017-04-14 14:45:44 -0400

    Include stdout and container_name in results

--- spec/support/test_container.rb
+++ spec/support/test_container.rb
@@ -10,6 +10,6 @@ class TestContainer
 
   def run(*)
     @outputs.each { |output| @on_output.call(output) }
-    ::CC::Analyzer::Container::Result.new(0, false, 1, false, 10, "")
+    ::CC::Analyzer::Container::Result.new(0, false, 1, false, 10, "", "", "")
   end
 end
