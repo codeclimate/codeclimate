bddaf72fd33bed1989f710e8c0296717ef48668a
patrick brisbin <pbrisbin@gmail.com>
2017-04-14 11:03:54 -0400

    Use Analyzer.logger, not CLI.debug
    - Give CLI a proper logger at the right level, use throughout
    - Assign Analyzer.logger from it for analysis logging

--- lib/cc/workspace/path_tree/dir_node.rb
+++ lib/cc/workspace/path_tree/dir_node.rb
@@ -42,7 +42,7 @@ module CC
             children[entry.basename.to_s.dup.freeze] ||= PathTree.node_for_pathname(entry)
             children[entry.basename.to_s.dup.freeze].add(*tail)
           else
-            CLI.debug("Couldn't include because part of path doesn't exist.", path: File.join(root_path, head))
+            Analyzer.logger.debug("Couldn't include because part of path doesn't exist: #{File.join(root_path, head)}")
           end
         end
 
