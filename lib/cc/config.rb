f200778dc4dc6984a54210154b3f5014eeaf2b69
patrick brisbin <pbrisbin@gmail.com>
2017-04-17 18:11:40 -0400

    Add CC::Config::YAML
    - Load configuration in /code working directory
    - Don't write an empty configuration to /config.json

--- lib/cc/config.rb
+++ lib/cc/config.rb
@@ -1,7 +1,16 @@
 require "cc/config/default"
 require "cc/config/engine"
+require "cc/config/yaml"
 
 module CC
   module Config
+    def self.load
+      @config ||=
+        if File.exist?(YAML::DEFAULT_PATH)
+          YAML.new
+        else
+          Default.new
+        end
+    end
   end
 end
