2df669fa66f8e24f1759efc02ee53474fe3baf66
patrick brisbin <pbrisbin@gmail.com>
2017-04-18 20:24:43 -0400

    Auto-enable plugins
    For engines which have auto_enable_paths in the manifest, check if any of the
    paths exists. If one is found, (auto-) enable the corresponding engine and set
    its config.file to the found path.

---
+++ lib/cc/analyzer/plugins.rb
@@ -0,0 +1,28 @@
+module CC
+  module Analyzer
+    class Plugins
+      def initialize
+        # For now, always use our registry because it's where auto_enable_paths
+        # live. This means we can never auto-enable (e.g.) brakeman-pro.
+        @registry = CC::EngineRegistry.new
+      end
+
+      def auto_enable_engines(config)
+        registry.each_engine do |engine, engine_details|
+          engine_details.auto_enable_paths.each do |path|
+            if File.exist?(path)
+              engine.enabled = true
+              engine.config["config"] = path
+              config.engines << engine
+              break
+            end
+          end
+        end
+      end
+
+      private
+
+      attr_reader :registry
+    end
+  end
+end
