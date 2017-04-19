580a64e70f5895dd75fe9d3c4433f7b8b310b6ef
patrick brisbin <pbrisbin@gmail.com>
2017-04-19 19:41:11 -0400

    Don't memoize in CC::Config#load
    This isn't useful and can sometimes leak state across tests.

--- lib/cc/config.rb
+++ lib/cc/config.rb
@@ -7,12 +7,11 @@ require "cc/config/yaml/validator"
 module CC
   module Config
     def self.load
-      @config ||=
-        if File.exist?(YAML::DEFAULT_PATH)
-          YAML.new
-        else
-          Default.new
-        end
+      if File.exist?(YAML::DEFAULT_PATH)
+        YAML.new
+      else
+        Default.new
+      end
     end
   end
 end
