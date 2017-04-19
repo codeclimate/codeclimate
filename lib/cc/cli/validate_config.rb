f28ba95edac9cf3fd503f6823be1e19691463347
patrick brisbin <pbrisbin@gmail.com>
2017-04-19 09:03:53 -0400

    Fixes to validate-config
    UX/UI:
    
    - Exit non-zero if ERRORs are found, so this works as a fail-able Build step
    - Don't color whole lines, makes them harder to read
    - Don't color success messages, it was also hard to read
    - Don't color WARNINGs red, I think this was a bug
    - Filter validation issues that are expected in QM-land
    
    Functionality:
    
    - Allow injecting registry (undocumented) so we can run this "normally" via
      Builder instead of having to require and invoke classes ourselves (and fake
      our usual container logging, snapshot-events, etc, etc).
    
    Implementation:
    
    - Encapsulate through a class under CC::Config, so we have only a single place
      to change as we move from CC::Yaml to the new CC::Config-based logic to
      support Quality Model.

--- lib/cc/cli/validate_config.rb
+++ lib/cc/cli/validate_config.rb
@@ -4,94 +4,62 @@ module CC
   module CLI
     class ValidateConfig < Command
       SHORT_HELP = "Validate your .codeclimate.yml.".freeze
-
-      include CC::Analyzer
-      include CC::Yaml
-
       VALID_CONFIG_MESSAGE = "No errors or warnings found in .codeclimate.yml file.".freeze
 
       def run
         require_codeclimate_yml
-        verify_yaml
+        process_args
+
+        if any_issues?
+          display_issues
+        else
+          puts VALID_CONFIG_MESSAGE
+        end
+
+        exit 1 unless validator.valid?
       end
 
       private
 
-      def verify_yaml
-        if any_issues?
-          display_issues
-        else
-          puts colorize(VALID_CONFIG_MESSAGE, :green)
+      attr_reader :config, :registry, :validator
+
+      def process_args
+        registry_path = EngineRegistry::DEFAULT_MANIFEST_PATH
+        registry_prefix = ""
+
+        # Undocumented; we only need these from Builder so we can validate
+        # engines/channels against our own registry and prefix.
+        while (arg = @args.shift)
+          case arg
+          when "--registry" then registry_path = @args.shift
+          when "--registry-prefix" then registry_prefix = @args.shift
+          end
         end
+
+        @validator = Config::YAML::Validator.new(
+          Config::YAML::DEFAULT_PATH,
+          EngineRegistry.new(registry_path, registry_prefix),
+        )
       end
 
       def any_issues?
-        parsed_yaml.errors? || parsed_yaml.nested_warnings.any? || parsed_yaml.warnings? || invalid_engines.any?
-      end
-
-      def yaml_content
-        filesystem.read_path(CODECLIMATE_YAML).freeze
-      end
-
-      def parsed_yaml
-        @parsed_yaml ||= CC::Yaml.parse(yaml_content)
-      end
-
-      def warnings
-        @warnings ||= parsed_yaml.warnings
-      end
-
-      def nested_warnings
-        @nested_warnings ||= parsed_yaml.nested_warnings
-      end
-
-      def errors
-        @errors ||= parsed_yaml.errors
+        validator.errors.any? ||
+          validator.warnings.any? ||
+          validator.nested_warnings.any?
       end
 
       def display_issues
-        display_errors
-        display_warnings
-        display_invalid_engines
-        display_nested_warnings
-      end
-
-      def display_errors
-        errors.each do |error|
-          puts colorize("ERROR: #{error}", :red)
+        validator.errors.each do |error|
+          puts "#{colorize("ERROR", :red)}: #{error}"
         end
-      end
 
-      def display_nested_warnings
-        nested_warnings.each do |nested_warning|
-          if nested_warning[0][0]
-            puts colorize("WARNING in #{nested_warning[0][0]}: #{nested_warning[1]}", :red)
-          end
+        validator.warnings.each do |warning|
+          puts "#{colorize("WARNING", :yellow)}: #{warning}"
         end
-      end
 
-      def display_warnings
-        warnings.each do |warning|
-          puts colorize("WARNING: #{warning}", :red)
+        validator.nested_warnings.each do |warning|
+          puts "#{colorize("WARNING in #{warning.field}", :yellow)}: #{warning.message}"
         end
-      end
-
-      def display_invalid_engines
-        invalid_engines.each do |engine_name|
-          puts colorize("WARNING: unknown engine <#{engine_name}>", :red)
-        end
-      end
-
-      def invalid_engines
-        @invalid_engines ||= engine_names.reject { |engine_name| engine_registry.exists? engine_name }
-      end
-
-      def engine_names
-        @engine_names ||= engines.keys
-      end
-
-      def engines
-        @engines ||= parsed_yaml.engines || {}
       end
     end
   end
--- spec/support/factory.rb
+++ spec/support/factory.rb
@@ -33,53 +33,9 @@ module Factory
     }
   end
 
-  def create_yaml_with_errors
-    %{
-      engkxhfgkxfhg: sdoufhsfogh: -
-      0-
-      fgkjfhgkdjfg;h:;
-        sligj:
-      oi i ;
-    }
-  end
-
-  def create_yaml_with_warning
-    %{
-      engines:
-      unknown_key:
-    }
-  end
-
-  def create_yaml_with_nested_warning
-    %{
-      engines:
-        rubocop:
-    }
-  end
-
-  def create_yaml_with_nested_and_unnested_warnings
-    %{
-      engines:
-        rubocop:
-          enabled: true
-        jshint:
-          not_enabled
-      strange_key:
-    }
-  end
-
   def create_yaml_with_no_engines
     %{
       engines:
-    }
-  end
-
-  def create_classic_yaml
-    %{
-      languages:
-        Ruby: true
-      exclude_paths:
-        - excluded.rb
     }
   end
 
