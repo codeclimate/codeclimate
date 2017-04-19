a8fcff1ee6afd56e3b4f5f0331862b19e5545425
patrick brisbin <pbrisbin@gmail.com>
2017-04-19 09:03:53 -0400

    Fixes to validate-config
    UX/UI:
    
    - Exit non-zero if ERRORs are found, so this works as a fail-able Build step
    - Don't color whole lines, makes them harder to read
    - Don't color success messages, it was also hard to read
    - Don't color WARNINGs red, I think this was a bug
    - Filter validation issues that are expected in QM-land
    
    Functionality:
    
    - Allow injecting registry (undocumented) so we can run this "normally" via
      Builder instead of having to require and invoke classes ourselves (and fake
      our usual container logging, snapshot-events, etc, etc).
    
    Implementation:
    
    - Encapsulate through a class under CC::Config, so we have only a single place
      to change as we move from CC::Yaml to the new CC::Config-based logic to
      support Quality Model.

--- spec/cc/cli/validate_config_spec.rb
+++ spec/cc/cli/validate_config_spec.rb
@@ -2,145 +2,115 @@ require "spec_helper"
 
 module CC::CLI
   describe ValidateConfig do
+    around do |spec|
+      Dir.chdir(Dir.mktmpdir) { spec.run }
+    end
+
     describe "#run" do
-      describe "when a .codeclimate.yml file is present in working directory" do
-        it "analyzes the .codeclimate.yml file without altering it" do
-          within_temp_dir do
-            expect(filesystem.exist?(".codeclimate.yml")).to eq(false)
+      it "reports errors and exits nonzero" do
+        write_cc_yaml(<<-EOYAML)
+        engkxhfgkxfhg: sdoufhsfogh: -
+        0-
+        fgkjfhgkdjfg;h:;
+          sligj:
+        oi i ;
+        EOYAML
 
-            yaml_content_before = "This is a test yaml!"
-            File.write(".codeclimate.yml", yaml_content_before)
-
-            expect(filesystem.exist?(".codeclimate.yml")).to eq(true)
-
-            capture_io do
-              validate_config = ValidateConfig.new
-              validate_config.run
-            end
-
-            expect(filesystem.exist?(".codeclimate.yml")).to eq(true)
-
-            content_after = File.read(".codeclimate.yml")
-
-            expect(content_after).to eq(yaml_content_before)
-          end
+        stdout, _stderr, code = capture_io_and_exit_code do
+          ValidateConfig.new.run
         end
 
-        describe "when there are errors present" do
-          it "reports that an error was found" do
-            within_temp_dir do
-              yaml_content = Factory.create_yaml_with_errors
-              File.write(".codeclimate.yml", yaml_content)
+        expect(stdout).to match("ERROR")
+        expect(code).to be_nonzero
+      end
 
-              stdout, stderr = capture_io do
-                ValidateConfig.new.run
-              end
+      it "reports warnings but does not exit nonzero" do
+        write_cc_yaml(<<-EOYAML)
+        unknown_key:
+        - hey
+        - there
+        EOYAML
 
-              expect(stdout).to match("ERROR")
-            end
-          end
+        stdout, _stderr, code = capture_io_and_exit_code do
+          ValidateConfig.new.run
         end
 
-        describe "when there are warnings present" do
-          it "reports that a warning was found" do
-            within_temp_dir do
-              yaml_content = Factory.create_yaml_with_warning
-              File.open(".codeclimate.yml", "w") do |f|
-                f.write(yaml_content)
-              end
+        expect(stdout).to match("WARNING")
+        expect(code).to be_zero
+      end
 
-              stdout, stderr = capture_io do
-                ValidateConfig.new.run
-              end
+      it "reports warnings in nested keys" do
+        write_cc_yaml(<<-EOYAML)
+        engines:
+          rubocop:
+        EOYAML
 
-              expect(stdout).to match("WARNING:")
-            end
-          end
+        stdout, _stderr, code = capture_io_and_exit_code do
+          ValidateConfig.new.run
         end
 
-        describe "when there are nested warnings present" do
-          it "reports that a warning was found in the parent item" do
-            within_temp_dir do
-              yaml_content = Factory.create_yaml_with_nested_warning
-              File.write(".codeclimate.yml", yaml_content)
+        expect(stdout).to match("invalid \"engines\" section: invalid \"rubocop\" section: missing key \"enabled\"")
+        expect(code).to be_nonzero
+      end
 
-              stdout, stderr = capture_io do
-                ValidateConfig.new.run
-              end
+      it "reports errors and nested warnings together" do
+        write_cc_yaml(<<-EOYAML)
+        engines:
+          rubocop:
+            enabled: true
+          jshint:
+            not_enabled
+        strange_key:
+        EOYAML
 
-              expect(stdout).to match("ERROR: invalid \"engines\" section")
-            end
-          end
+        stdout, _stderr, code = capture_io_and_exit_code do
+          ValidateConfig.new.run
         end
 
-        describe "when there are both regular and nested warnings present" do
-          it "reports both kinds of warnings" do
-            within_temp_dir do
-              yaml_content = Factory.create_yaml_with_nested_and_unnested_warnings
-              File.write(".codeclimate.yml", yaml_content)
+        expect(stdout).to match("invalid \"engines\" section: invalid \"jshint\" section: unexpected scalar, missing key \"enabled\"")
+        expect(stdout).to match("unexpected key \"strange_key\", dropping")
+        expect(code).to be_nonzero
+      end
 
-              stdout, stderr = capture_io do
-                ValidateConfig.new.run
-              end
+      it "reports copy looks great for valid configs" do
+        write_cc_yaml(<<-EOYAML)
+        engines:
+          rubocop:
+            enabled: true
+        EOYAML
 
-              expect(stdout).to match("ERROR: invalid \"engines\" section")
-            end
-          end
+        stdout, _stderr, code = capture_io_and_exit_code do
+          ValidateConfig.new.run
         end
 
-        describe "when the present yaml is valid" do
-          it "reports copy looks great" do
-            within_temp_dir do
-              yaml_content = Factory.create_correct_yaml
-              File.write(".codeclimate.yml", yaml_content)
+        expect(stdout).to match(/no errors or warnings/i)
+        expect(code).to be_zero
+      end
 
-              stdout, stderr = capture_io do
-                ValidateConfig.new.run
-              end
+      it "warns of invalid engines or channels" do
+        write_cc_yaml(<<-EOYAML)
+        engines:
+          rubocop:
+            enabled: true
+          duplication:
+            enabled: true
+            channel: madeup
+          madeup:
+            enabled: true
+        EOYAML
 
-              expect(stdout).to match("No errors or warnings found in .codeclimate.yml file.")
-            end
-          end
+        stdout, _stderr, code = capture_io_and_exit_code do
+          ValidateConfig.new.run
         end
 
-        describe "when there are invalid engines" do
-          it "reports that those engines are invalid" do
-            within_temp_dir do
-              yaml_content = <<-YAML
-                engines:
-                  rubocop:
-                    enabled: true
-                  madeup:
-                    enabled: true
-                ratings:
-                  paths:
-                  - "**/*.rb"
-                  - "**/*.js"
-              YAML
-
-              File.write(".codeclimate.yml", yaml_content)
-
-              stdout, stderr = capture_io do
-                ValidateConfig.new.run
-              end
-
-              expect(stdout).to include("WARNING: unknown engine <madeup>")
-            end
-          end
-        end
+        expect(stdout).to include("unknown engine or channel <madeup:stable>")
+        expect(stdout).to include("unknown engine or channel <duplication:madeup>")
+        expect(code).to be_zero
       end
     end
 
-    def filesystem
-      @filesystem ||= CC::Analyzer::Filesystem.new(".")
-    end
-
-    def within_temp_dir(&block)
-      temp = Dir.mktmpdir
-
-      Dir.chdir(temp) do
-        yield
-      end
+    def write_cc_yaml(content)
+      File.write(".codeclimate.yml", content)
     end
   end
 end
