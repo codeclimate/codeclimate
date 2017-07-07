1421e10738a95e9e137632d5cc7fe88509c2f256
Will Fleming <will@flemi.ng>
2017-07-07 14:56:38 -0400

    Support "plugins" in JSON config (#694)
    * Support "plugins" in JSON config
    
    This is the new key for specifying plugin engines.
    
    Since we'll shortly be ignoring legacy yaml entirely when JSON is
    present, we also have a practical need to be able to configure
    structure/duplication in this file (e.g. to set channel), so this will
    respect configuration for those engines under the "plugins" key, even
    though they are not semantically plugins.
    
    * lean more on existing merge functionality
    
    * simplify, simplify
    
    * doh

--- lib/cc/config/json.rb
+++ lib/cc/config/json.rb
@@ -8,7 +8,7 @@ module CC
         @json = ::JSON.parse(File.open(path).read) || {}
 
         super(
-          engines: Set.new([duplication_engine, structure_engine]),
+          engines: Set.new([duplication_engine, structure_engine] + plugin_engines),
           exclude_patterns: json.fetch("exclude_patterns", Default::EXCLUDE_PATTERNS)
         )
       end
@@ -23,6 +23,7 @@ module CC
         Engine.new(
           "structure",
           enabled: true,
+          channel: channel_overrides.fetch("structure", "stable"),
           config: {
             "config" => {
               "checks" => json.fetch("checks", {}),
@@ -35,7 +36,7 @@ module CC
         Engine.new(
           "duplication",
           enabled: true,
-          channel: "cronopio",
+          channel: channel_overrides.fetch("duplication", "cronopio"),
           config: {
             "config" => {
               "languages" => Default::DUPLICATION_LANGUAGES,
@@ -43,6 +44,25 @@ module CC
             },
           },
         )
+      end
+
+      def plugin_engines
+        json.fetch("plugins", {}).map do |name, data|
+          plugin_engine(name, data)
+        end
+      end
+
+      def plugin_engine(name, data)
+        Engine.new(
+          name,
+          enabled: data.fetch("enabled", true),
+          channel: data["channel"],
+          config: data
+        )
+      end
+
+      def channel_overrides
+        json.fetch("channels", {})
       end
     end
   end
