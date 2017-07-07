e13dfd8c3841fbe3482255debf5a852b7fbb8ab5
Will Fleming <will@flemi.ng>
2017-07-07 11:53:46 -0400

    Support "plugins" in JSON config
    This is the new key for specifying plugin engines.
    
    Since we'll shortly be ignoring legacy yaml entirely when JSON is
    present, we also have a practical need to be able to configure
    structure/duplication in this file (e.g. to set channel), so this will
    respect configuration for those engines under the "plugins" key, even
    though they are not semantically plugins.

--- lib/cc/config/json.rb
+++ lib/cc/config/json.rb
@@ -8,7 +8,7 @@ module CC
         @json = ::JSON.parse(File.open(path).read) || {}
 
         super(
-          engines: Set.new([duplication_engine, structure_engine]),
+          engines: Set.new([duplication_engine, structure_engine] + plugin_engines),
           exclude_patterns: json.fetch("exclude_patterns", Default::EXCLUDE_PATTERNS)
         )
       end
@@ -20,29 +20,69 @@ module CC
         :json
 
       def structure_engine
-        Engine.new(
-          "structure",
-          enabled: true,
-          config: {
-            "config" => {
-              "checks" => json.fetch("checks", {}),
+        override = all_configured_engines.detect { |engine| engine.name == "structure" }
+
+        if override
+          set_engine_checks(override, json.fetch("checks", {}))
+          override
+        else
+          Engine.new(
+            "structure",
+            enabled: true,
+            config: {
+              "config" => {
+                "checks" => json.fetch("checks", {}),
+              },
             },
-          },
-        )
+          )
+        end
       end
 
       def duplication_engine
-        Engine.new(
-          "duplication",
-          enabled: true,
-          channel: "cronopio",
-          config: {
-            "config" => {
-              "languages" => %w[javascript ruby],
-              "checks" => json.fetch("checks", {}),
+        override = all_configured_engines.detect { |engine| engine.name == "duplication" }
+
+        if override
+          set_engine_checks(override, json.fetch("checks", {}))
+          override
+        else
+          Engine.new(
+            "duplication",
+            enabled: true,
+            channel: "cronopio",
+            config: {
+              "config" => {
+                "languages" => %w[javascript ruby],
+                "checks" => json.fetch("checks", {}),
+              },
             },
-          },
+          )
+        end
+      end
+
+      def plugin_engines
+        all_configured_engines.reject do |engine|
+          %w[structure duplication].include?(engine.name)
+        end
+      end
+
+      def all_configured_engines
+        @all_configured_engines ||= json.fetch("plugins", {}).map do |name, data|
+          plugin_engine(name, data)
+        end
+      end
+
+      def plugin_engine(name, data)
+        Engine.new(
+          name,
+          enabled: data.fetch("enabled", true),
+          channel: data["channel"],
+          config: data
         )
+      end
+
+      def set_engine_checks(engine, checks)
+        engine.config["config"] ||= {}
+        engine.config["config"]["checks"] ||= checks
       end
     end
   end
--- spec/cc/config/json_spec.rb
+++ spec/cc/config/json_spec.rb
@@ -23,6 +23,63 @@ describe CC::Config::JSON do
         )
       end
     end
+
+    it "includes specified plugins" do
+      json = load_cc_json(<<-EOS)
+        {
+          "plugins": {
+            "rubocop": {
+              "enabled": false,
+              "channel": "beta",
+              "config": { "something": "else" }
+            }
+          }
+        }
+      EOS
+
+      expect(json.engines.count).to eq(3)
+
+      engine = json.engines.detect { |engine| engine.name == "rubocop" }
+      expect(engine).not_to be_nil
+      expect(engine.enabled?).to eq(false)
+      expect(engine.channel).to eq("beta")
+      expect(engine.config).to eq({
+        "enabled" => false,
+        "channel" => "beta",
+        "config" => { "something" => "else" },
+      })
+    end
+
+    it "respects overrides of core engines" do
+      json = load_cc_json(<<-EOS)
+        {
+          "checks": {
+            "cyclomatic-complexity": {
+              "enabled": true
+            }
+          },
+          "plugins": {
+            "structure": {
+              "enabled": false,
+              "channel": "beta",
+              "config": { "something": "else" }
+            }
+          }
+        }
+      EOS
+
+      expect(json.engines.count).to eq(2)
+
+      engine = json.engines.detect { |engine| engine.name == "structure" }
+      expect(engine).not_to be_nil
+      expect(engine.enabled?).to eq(false)
+      expect(engine.channel).to eq("beta")
+      expect(engine.config["config"]["checks"]).to eq(
+        "cyclomatic-complexity" => {
+          "enabled" => true,
+        },
+      )
+    end
   end
 
   describe "#exclude_paths" do
