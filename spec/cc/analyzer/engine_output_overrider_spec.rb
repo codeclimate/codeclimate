098aa5751369858895916d72bf5d69ebd689648a
patrick brisbin <pbrisbin@gmail.com>
2017-07-07 12:15:16 -0400

    Update output override related specs

--- spec/cc/analyzer/engine_output_overrider_spec.rb
+++ spec/cc/analyzer/engine_output_overrider_spec.rb
@@ -4,7 +4,7 @@ module CC
   module Analyzer
     describe EngineOutputOverrider do
       it "does not modify arbitrary json" do
-        output = EngineOutput.new(%({"arbitrary":"json"}))
+        output = EngineOutput.new("", %({"arbitrary":"json"}))
         expect(subject.apply(output)).to eq output
       end
 
@@ -12,18 +12,17 @@ module CC
         issue = build_issue("severity" => "major")
 
         overrider = described_class.new(
-          engine_config(
-            "issue_override" => {
-              "severity" => "info",
-            },
-          ),
+          "enabled" => true,
+          "issue_override" => {
+            "severity" => "info",
+          },
         )
 
         expect(overrider.apply(issue)["severity"]).to eq "info"
       end
 
       def build_issue(attributes)
-        EngineOutput.new({
+        EngineOutput.new("", {
           "type" => EngineOutputFilter::ISSUE_TYPE,
           "check_name" => "rubocop",
           "location" => {
@@ -31,16 +30,6 @@ module CC
             "lines" => { "begin" => 13, "end" => 14 },
           },
         }.merge(attributes).to_json)
-      end
-
-      def engine_config(hash)
-        codeclimate_yaml = {
-          "engines" => {
-            "rubocop" => hash.merge("enabled" => true),
-          },
-        }.to_yaml
-
-        CC::Yaml.parse(codeclimate_yaml).engines["rubocop"]
       end
     end
   end
