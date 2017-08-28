4d454cde12b2fa6dc0ab730837ae04ab42622624
Ale Paredes <alexandra@codeclimate.com>
2017-08-28 11:04:45 -0400

    Include engine name on raising container errors (#716)

--- lib/cc/analyzer/raising_container_listener.rb
+++ lib/cc/analyzer/raising_container_listener.rb
@@ -11,16 +11,16 @@ module CC
         if result.timed_out?
           message = "engine #{engine.name} ran for #{result.duration / 1000}"
           message << " seconds and was killed"
-          raise timeout_ex, message
+          raise timeout_ex.new(message, engine.name)
         elsif result.maximum_output_exceeded?
           message = "engine #{engine.name} produced too much output"
           message << " (#{result.output_byte_count} bytes)"
-          raise maximum_output_ex, message
+          raise maximum_output_ex.new(message, engine.name)
         elsif result.exit_status.nonzero?
           message = "engine #{engine.name} failed"
           message << " with status #{result.exit_status}"
           message << " and stderr \n#{result.stderr}"
-          raise failure_ex, message
+          raise failure_ex.new(message, engine.name)
         end
       end
 
