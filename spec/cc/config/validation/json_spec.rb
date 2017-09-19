17b9b263868811e5471cc815eed12e96fae0c1b7
Will Fleming <will@flemi.ng>
2017-09-19 18:00:54 -0400

    Add JSON validation
    Implement same necessary validations for JSON, and update the
    `ValidateConfig` command to validate JSON as appropriate.

---
+++ spec/cc/config/validation/json_spec.rb
@@ -0,0 +1,214 @@
+require "spec_helper"
+
+describe CC::Config::Validation::JSON do
+  it "is valid for a complete config" do
+    validator = validate_json(<<-EOJSON)
+    {
+      "prepare": {
+        "fetch": [
+          "http://test.test/rubocop.yml",
+          {
+            "url": "http://test.test/myeslint.json",
+            "path": "eslint.json"
+          }
+        ]
+      },
+      "plugins": {
+        "rubocop": {
+          "enabled": true,
+          "channel": "beta",
+          "config": { "file": "foobar" }
+        },
+        "eslint": {
+          "enabled": true,
+          "config": "bazfoo"
+        },
+        "hlint": true
+      },
+      "exclude_patterns": [
+        "**/*.rb",
+        "foo/"
+      ]
+    }
+    EOJSON
+
+    expect(validator).to be_valid
+    expect(validator.warnings.length).to eq(0)
+  end
+
+  it "handles unparseable json" do
+    validator = validate_json("{")
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include(/Unable to parse:/)
+  end
+
+  it "handles parseable but non-hash json" do
+    validator = validate_json(%{["foo", "bar"]})
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include(/Config file should contain a hash/)
+  end
+
+  it "reports error for hash fetch" do
+    validator = validate_json(<<-EOJSON)
+    {
+      "prepare": {
+        "fetch": {
+          "rubocop": "http://test.test/rubocop.yml"
+        }
+      }
+    }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include("prepare section: 'fetch' must be an array")
+  end
+
+  it "reports errors for fetch with invalid URL or missing path" do
+    validator = validate_json(<<-EOJSON)
+    {
+      "prepare": {
+        "fetch": [
+          "test.test/rubocop.yml",
+          { "url": "http://test.test/myeslint.json" }
+        ]
+      }
+    }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include(%r{invalid URL 'test.test/rubocop.yml'})
+    expect(validator.errors).to include(/must include 'url' & 'path'/)
+  end
+
+  it "reports errors for fetch with invalid paths" do
+    validator = validate_json(<<-EOJSON)
+    {
+      "prepare": {
+        "fetch": [
+          {
+            "url": "http://test.test/myeslint.json",
+            "path": null
+          },
+          {
+            "url": "http://test.test/myeslint.json",
+            "path": "/etc/passwd"
+          },
+          {
+            "url": "http://test.test/myeslint.json",
+            "path": "../../htaccess"
+          }
+        ]
+      }
+    }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include(/'path' cannot be empty/)
+    expect(validator.errors).to include(%r{absolute path '/etc/passwd' is invalid})
+    expect(validator.errors).to include(%r{relative path elements in '../../htaccess' are invalid})
+  end
+
+  it "reports errors for array plugins" do
+    validator = validate_json(<<-EOJSON)
+    { "plugins": [ "rubocop" ] }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include(/'plugins' must be a hash/)
+  end
+
+  it "reports errors for invalid stringy engine config" do
+    validator = validate_json(<<-EOJSON)
+    { "plugins": { "rubocop": "foobar" } }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include("engine rubocop: section must be a boolean or a hash")
+  end
+
+  it "reports errors for engine config with invalid contents" do
+    validator = validate_json(<<-EOJSON)
+    {
+      "plugins": {
+        "rubocop": {
+          "enabled": "foobar",
+          "config": false
+        }
+      }
+    }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include("engine rubocop: 'enabled' must be a boolean")
+    expect(validator.errors).to include("engine rubocop: 'config' must be one of string, hash")
+  end
+
+  it "reports errors for array checks" do
+    validator = validate_json(<<-EOJSON)
+    { "checks": [ "foo" ] }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include(/'checks' must be a hash/)
+  end
+
+  it "reports errors for check with boolean config" do
+    validator = validate_json(<<-EOJSON)
+    { "checks": { "foo": false } }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include("check foo: must be a hash")
+  end
+
+  it "reports errors for check with invalid config" do
+    validator = validate_json(<<-EOJSON)
+    {
+      "checks": {
+        "foo": {
+          "enabled": "foobar",
+          "config": false
+        }
+      }
+    }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include("check foo: 'enabled' must be a boolean")
+    expect(validator.errors).to include("check foo: 'config' must be a hash")
+  end
+
+  it "reports errors for singular string exclude patterns" do
+    validator = validate_json(<<-EOJSON)
+    { "exclude_patterns": "foobar" }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include("'exclude_patterns' must be an array")
+  end
+
+  it "reports errors for errors within exclude_patterns" do
+    validator = validate_json(<<-EOJSON)
+    {
+      "exclude_patterns": [
+        { "foo": "bar" }
+      ]
+    }
+    EOJSON
+
+    expect(validator).not_to be_valid
+    expect(validator.errors).to include(/each exclude pattern should be a string/)
+  end
+
+  def validate_json(json, registry = nil)
+    Tempfile.open("") do |tmp|
+      tmp.puts(json)
+      tmp.rewind
+
+      registry ||= double(:engine_registry, fetch_engine_details: {})
+      described_class.new(tmp.path, registry)
+    end
+  end
+end
