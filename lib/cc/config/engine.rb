ab7b0dee727781c36da5ec9fba383d504c2d5972
Will Fleming <will@flemi.ng>
2017-09-19 11:55:51 -0400

    Config: make defaults an adapter
    Another step towards config handling being a single sequence of steps.
    Defaults can be applied on top of whatever was already sourced from an
    existing config file.
    
    There are some nice simplifications I did because they were easy: e.g.
    `duplication` no longer requires `languages` in the config, so we don't
    need to set it.
    
    This also helps fix some confusing behavior we've seen internally with
    trying to override channels/enabled status for QM engines, and paves the
    way for similar support for per-engine exclude_patterns, which we'll
    restore soon. It's not *perfect* that we put these engines under
    `plugins`, since they're not "plugins", but for now I think it's fine
    that this is an implementation detail: customers won't tweak these bits
    as much, and the technical benefits of treating these engines the same
    as others is pretty large.

--- lib/cc/config/engine.rb
+++ lib/cc/config/engine.rb
@@ -6,18 +6,6 @@ module CC
       attr_reader :name, :channel, :config
       attr_writer :enabled
 
-      def self.list_from_data(data)
-        engines = data.map do |name, engine_data|
-          Engine.new(
-            name,
-            enabled: engine_data.fetch("enabled", true),
-            channel: engine_data["channel"],
-            config: engine_data
-          )
-        end
-        Set.new(engines)
-      end
-
       def initialize(name, enabled: false, channel: nil, config: nil)
         @name = name
         @enabled = enabled
@@ -30,32 +18,18 @@ module CC
       end
 
       def plugin?
-        !Default::ENGINE_NAMES.include?(name)
+        !DefaultAdapter::ENGINES.keys.include?(name)
       end
 
       def container_label
         @container_label ||= SecureRandom.uuid
       end
 
-      def merge(other)
-        if eql?(other)
-          self.class.new(name, enabled: other.enabled?, channel: other.channel, config: config.deep_merge(other.config))
-        else
-          raise ArgumentError, "Engine names must match to merge"
-        end
-      end
-
-      # Set interface methods. Assumes we never want to store the same engine by
-      # name in the same list. This should be true except maybe if we want to
-      # work with multiple channels at once, which is unlikely.
-
       def eql?(other)
         other.is_a?(self.class) && name.eql?(other.name)
       end
-
-      def hash
-        name.hash
-      end
+      alias_method :==, :eql?
+      alias_method :equal?, :eql?
     end
   end
 end
