892f088b4329e8a4c9b0b1f456c5cc96fa7d1a97
Will Fleming <will@flemi.ng>
2017-09-19 17:26:06 -0400

    Rewrite YAML validation
    Our schema is sufficiently different under QM that the old validation
    checks are not terribly helpful. We've intended to abandon
    `codeclimate-yaml` for some time, since it's type-coercion behavior has
    been troublesome. Addtionally, we'll want to be add validation of the
    JSON schema & would like to share a good deal of logic between the two
    formats since they're closely related.
    
    This does the first necessary part of that, re-writing our config
    validation within this repo, and porting over some logic that was useful
    (e.g. prepare fetch validation details).

---
+++ lib/cc/config/validation/prepare_validator.rb
@@ -0,0 +1,38 @@
+module CC
+  class Config
+    module Validation
+      class PrepareValidator
+        include HashValidations
+
+        attr_reader :errors, :warnings
+
+        def initialize(data)
+          @data = data
+
+          @errors = []
+          @warnings = []
+
+          validate
+        end
+
+        private
+
+        attr_reader :data
+
+        def validate
+          return unless validate_key_type("fetch", Array)
+
+          data.fetch("fetch", []).each do |fetch_data|
+            validator = FetchValidator.new(fetch_data)
+            validator.errors.each do |msg|
+              errors << msg
+            end
+            validator.warnings.each do |msg|
+              warnings << msg
+            end
+          end
+        end
+      end
+    end
+  end
+end
