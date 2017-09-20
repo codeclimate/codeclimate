340023aee9625233a475ab48bb99f6b3d6b94ed9
Will Fleming <will@flemi.ng>
2017-09-20 12:27:06 -0400

    Respect engine specific excludes when running the engine
    Added a small spec for `Bridge` as well since we didn't have one.

---
+++ spec/cc/analyzer/bridge_spec.rb
@@ -0,0 +1,170 @@
+require "spec_helper"
+
+describe CC::Analyzer::Bridge do
+  include FileSystemHelpers
+
+  around do |test|
+    within_temp_dir { test.call }
+  end
+
+  describe "#run" do
+    it "runs an engine" do
+      make_tree <<-EOFILES
+        foo/thing.rb
+        foo/bar.rb
+        bar/baz.rb
+      EOFILES
+
+      write_cc_yaml(YAML.dump(
+        "plugins" => { "foo" => true },
+        "exclude_patterns" => ["bar/"],
+      ))
+
+      expect_engine_run(
+        "structure",
+        { "image" => "structure-stable", "command" => nil },
+        {
+          "enabled" => true,
+          "channel" => "stable",
+          "include_paths" => ["foo/", ".codeclimate.yml", "engines.yml"],
+        },
+      )
+      expect_engine_run(
+        "duplication",
+        { "image" => "duplication:cronopio", "command" => nil },
+        {
+          "enabled" => true,
+          "channel" => "cronopio",
+          "include_paths" => ["foo/", ".codeclimate.yml", "engines.yml"],
+        },
+      )
+      expect_engine_run(
+        "foo",
+        { "image" => "foo-stable", "command" => nil },
+        {
+          "enabled" => true,
+          "channel" => "stable",
+          "include_paths" => ["foo/", ".codeclimate.yml", "engines.yml"],
+        },
+      )
+
+      described_class.new(
+        config: CC::Config.load,
+        formatter: stub_formatter,
+        listener: stub_listener,
+        registry: engine_registry,
+      ).run
+    end
+
+    it "runs engines, respecting engine exclude_patterns" do
+      make_tree <<-EOFILES
+        foo/thing.rb
+        foo/bar.rb
+        bar/baz.rb
+      EOFILES
+
+      write_cc_yaml(YAML.dump(
+        "plugins" => {
+          "foo" => { "exclude_patterns" => ["**/*r.rb"] },
+          "bar" => { "enabled" => true, "channel" => "beta" },
+        },
+        "exclude_patterns" => ["bar/"]
+      ))
+
+      expect_engine_run(
+        "structure",
+        { "image" => "structure-stable", "command" => nil },
+        {
+          "enabled" => true,
+          "channel" => "stable",
+          "include_paths" => ["foo/", ".codeclimate.yml", "engines.yml"],
+        },
+      )
+      expect_engine_run(
+        "duplication",
+        { "image" => "duplication:cronopio", "command" => nil },
+        {
+          "enabled" => true,
+          "channel" => "cronopio",
+          "include_paths" => ["foo/", ".codeclimate.yml", "engines.yml"],
+        },
+      )
+      expect_engine_run(
+        "foo",
+        { "image" => "foo-stable", "command" => nil },
+        {
+          "exclude_patterns" => ["**/*r.rb"],
+          "channel" => "stable",
+          "include_paths" => ["foo/thing.rb", ".codeclimate.yml", "engines.yml"],
+        },
+      )
+      expect_engine_run(
+        "bar",
+        { "image" => "bar:beta", "command" => nil },
+        {
+          "enabled" => true,
+          "channel" => "beta",
+          "include_paths" => ["foo/", ".codeclimate.yml", "engines.yml"],
+        },
+      )
+
+      described_class.new(
+        config: CC::Config.load,
+        formatter: stub_formatter,
+        listener: stub_listener,
+        registry: engine_registry,
+      ).run
+    end
+  end
+
+  def write_cc_yaml(yaml)
+    File.write(CC::Config::YAMLAdapter::DEFAULT_PATH, yaml)
+  end
+
+  def engine_registry
+    File.write("engines.yml", <<-EOYAML)
+    structure:
+      channels:
+        stable: structure-stable
+    duplication:
+      channels:
+        stable: duplication-stable
+        cronopio: "duplication:cronopio"
+    foo:
+      channels:
+        stable: foo-stable
+    bar:
+      channels:
+        stable: bar-stable
+        beta: "bar:beta"
+    EOYAML
+    CC::EngineRegistry.new("engines.yml")
+  end
+
+  def expect_engine_run(name, metadata, config)
+    engine_double = double("engine_#{name}")
+    expect(CC::Analyzer::Engine).to receive(:new).with(
+      name,
+      metadata,
+      config,
+      instance_of(String),
+    ).and_return(engine_double)
+    expect(engine_double).to receive(:run).once
+  end
+
+  def stub_formatter
+    double(:formatter).tap do |stub|
+      expect(stub).to receive(:started).once
+      expect(stub).to receive(:engine_running).at_least(:once).and_yield
+      expect(stub).to receive(:finished).once
+      expect(stub).to receive(:close).once
+    end
+  end
+
+  def stub_listener
+    double(:listener).tap do |stub|
+      expect(stub).to receive(:started).at_least(:once)
+      expect(stub).to receive(:finished).at_least(:once)
+    end
+  end
+end
