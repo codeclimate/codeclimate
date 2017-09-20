ea858ba23e9957cc1f699af18d3a12dad39af884
Will Fleming <will@flemi.ng>
2017-09-20 18:36:20 -0400

    Remove several engines subcommands
    These commands are all for modifying the committed
    `.codeclimate.yml`. However, whether or how to support them becomes more
    complicated with recent changes to config & engines.
    
    First, we now support config from within the web app, or from checked in
    YAML, or checked in JSON. So these commands would need to expand to also
    edit JSON files as appropriate. And there'd be the lingering questions
    of if/how these commands should interact with the in-app config if
    that's being used.
    
    Second, there's a real drawback to how these commands work with YAML
    today: they strip out comments & reformat your config file. That's
    pretty undesirable since being able to annotate your config with
    comments is a major benefit of YAML in general.
    
    Third, all these commands really do for you is edit 1 or 2 lines of your
    config file (and pull the engine, but that also happens automatically or
    via `engines:install`). Duplication used to require `config` contents,
    but it no longer does. The other default configs we were setting were
    either not needed or plain wrong, according to those engines' docs.
    
    So, in short, I see these commands as somewhat broken low value legacy
    debt. I think we can drop support for them with no real ill effects.

--- lib/cc/cli/engines.rb
+++ lib/cc/cli/engines.rb
@@ -1,8 +1,5 @@
 require "cc/analyzer"
 
-require "cc/cli/engines/disable"
-require "cc/cli/engines/enable"
 require "cc/cli/engines/engine_command"
 require "cc/cli/engines/install"
 require "cc/cli/engines/list"
-require "cc/cli/engines/remove"
