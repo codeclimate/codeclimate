9374915e7030288ba8d06d5cde45c571ea11cd40
Will Fleming <will@flemi.ng>
2017-09-21 13:09:24 -0400

    Rm unused Factory methods

--- spec/support/factory.rb
+++ spec/support/factory.rb
@@ -1,44 +1,6 @@
 module Factory
   extend self
 
-  def yaml_with_rubocop_enabled
-    %{
-    engines:
-      rubocop:
-        enabled: true
-    }
-  end
-
-  def yaml_without_rubocop_enabled
-    %{
-    engines:
-      rubocop:
-        enabled: false
-    }
-  end
-
-  def yaml_without_jshint
-  %{
-    engines:
-      rubocop:
-        enabled: false
-    }
-  end
-
-  def create_correct_yaml
-    %{
-      engines:
-        rubocop:
-          enabled: true
-    }
-  end
-
-  def create_yaml_with_no_engines
-    %{
-      engines:
-    }
-  end
-
   def sample_issue(options = {})
     {
       "type" => "issue",
