e36412cc45e69cb512406d01266352090a7c324d
Filipe Esperandio <filipesperandio@gmail.com>
2017-10-26 17:59:55 -0200

    Enable spec focus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,10 @@ Dir.glob("spec/support/**/*.rb").each(&method(:load))
 
 ENV.delete("CODECLIMATE_DOCKER")
 ENV["CODECLIMATE_TMP"] = Dir.mktmpdir
+
+RSpec.configure do |config|
+  config.before(:example, :focus) { raise "Should not commit focused specs" } if ENV["CIRCLECI"]
+  config.filter_run focus: true
+  config.alias_example_to :fit, focus: true
+  config.run_all_when_everything_filtered = true
+end
