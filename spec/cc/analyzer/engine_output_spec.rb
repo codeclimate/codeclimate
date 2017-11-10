988e63347db8aead43321c475f87d4bb03bb8923
Will Fleming <will@flemi.ng>
2017-11-10 11:32:42 -0500

    EngineOutput: catch invalid JSON
    Bad engine output should be treated as an engine error.

--- spec/cc/analyzer/engine_output_spec.rb
+++ spec/cc/analyzer/engine_output_spec.rb
@@ -9,5 +9,38 @@ module CC::Analyzer
         expect(EngineOutput.new("", output).issue?).to eq(true)
       end
     end
+
+    describe "#valid?" do
+      it "is true for a valid issue" do
+        output = sample_issue.to_json
+        expect(EngineOutput.new("engine", output)).to be_valid
+      end
+
+      it "is false for an invalid issue" do
+        output = { type: "issue", categories: ["Foo"] }.to_json
+        expect(EngineOutput.new("engine", output)).not_to be_valid
+      end
+
+      it "is false for invalid JSON" do
+        output = "{bad"
+        expect(EngineOutput.new("engine", output)).not_to be_valid
+      end
+    end
+
+    describe "#error" do
+      it "gets IssueValidator errors for invalid issue" do
+        output = { type: "issue", categories: ["Foo"] }.to_json
+        expect(EngineOutput.new("engine", output).error[:message]).to match(
+          "Category must be at least one of"
+        )
+      end
+
+      it "is appropriate for invalid JSON" do
+        output = "{bad"
+        expect(EngineOutput.new("engine", output).error[:message]).to match(
+          "Invalid JSON"
+        )
+      end
+    end
   end
 end
