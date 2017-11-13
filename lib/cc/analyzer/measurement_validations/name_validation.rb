6c0a578dc04f80171e0daf28eef178be3bba4c6f
Will Fleming <will@flemi.ng>
2017-11-13 16:49:03 -0500

    Add more allowed characters for measurement names

--- lib/cc/analyzer/measurement_validations/name_validation.rb
+++ lib/cc/analyzer/measurement_validations/name_validation.rb
@@ -2,14 +2,14 @@ module CC
   module Analyzer
     module MeasurementValidations
       class NameValidation < Validation
-        REGEX = /^[A-Za-z0-9_]+$/
+        REGEX = /^[A-Za-z0-9_\.\-]+$/
 
         def valid?
           name && name.is_a?(String) && REGEX.match?(name)
         end
 
         def message
-          "Name must be present and contain only letters, numbers, dots, and underscores"
+          "Name must be present and contain only letters, numbers, periods, hyphens, and underscores"
         end
 
         private
