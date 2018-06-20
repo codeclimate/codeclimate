395193c7a86992865d1e99416c04b35e372f43f9
Wilson Bilkovich <wilsonb@gmail.com>
2018-06-20 14:49:02 -0400

    Report per-channel metrics for engines that support channels. (#839)

--- lib/cc/analyzer/statsd_container_listener.rb
+++ lib/cc/analyzer/statsd_container_listener.rb
@@ -6,24 +6,24 @@ module CC
       end
 
       def started(engine, _details)
-        increment(engine.name, "started")
+        increment(engine, "started")
       end
 
       def finished(engine, _details, result)
-        timing(engine.name, "time", result.duration)
-        increment(engine.name, "finished")
+        timing(engine, "time", result.duration)
+        increment(engine, "finished")
 
         if result.timed_out?
-          timing(engine.name, "time", result.duration)
-          increment(engine.name, "result.error")
-          increment(engine.name, "result.error.timeout")
+          timing(engine, "time", result.duration)
+          increment(engine, "result.error")
+          increment(engine, "result.error.timeout")
         elsif result.maximum_output_exceeded?
-          increment(engine.name, "result.error")
-          increment(engine.name, "result.error.output_exceeded")
+          increment(engine, "result.error")
+          increment(engine, "result.error.output_exceeded")
         elsif result.exit_status.nonzero?
-          increment(engine.name, "result.error")
+          increment(engine, "result.error")
         else
-          increment(engine.name, "result.success")
+          increment(engine, "result.success")
         end
       end
 
@@ -31,14 +31,20 @@ module CC
 
       attr_reader :statsd
 
-      def increment(name, metric)
+      def increment(engine, metric)
         statsd.increment("engines.#{metric}")
-        statsd.increment("engines.names.#{name}.#{metric}")
+        statsd.increment("engines.names.#{engine.name}.#{metric}")
+        if engine.respond_to?(:channel) && engine.channel
+          statsd.increment("engines.names.#{engine.name}.#{engine.channel}.#{metric}")
+        end
       end
 
-      def timing(name, metric, ms)
+      def timing(engine, metric, ms)
         statsd.timing("engines.#{metric}", ms)
-        statsd.timing("engines.names.#{name}.#{metric}", ms)
+        statsd.timing("engines.names.#{engine.name}.#{metric}", ms)
+        if engine.respond_to?(:channel) && engine.channel
+          statsd.timing("engines.names.#{engine.name}.#{engine.channel}.#{metric}", ms)
+        end
       end
     end
   end
