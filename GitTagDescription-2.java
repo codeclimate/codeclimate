17b823414f92f505f669997fd7a58bbb9d62fe95
Emily Fueger <efueger@users.noreply.github.com>
2022-05-27 09:17:02 -0400

    Create GitTagDescription-2.java

---
+++ GitTagDescription-2.java
@@ -0,0 +1,27 @@
+
+CODE CLIMATE OPTIONS
+Show issues
+Show test coverage
+ Highlight covered lines
+   
+    @Override
+    public String toString() {
+TODO found
+      return "TODO: implement this method";
+    @Override 
+    public String toString() { 
+        if (this.nextTag == null) {
+            return this.abbreviatedCommitId;
+        } else if (this.distance == 0) {
+            return this.nextTag.getName();
+        } else {
+            if (!this.nextTag.getName().isEmpty() &&
+                this.distance != 0) {
+                      return String.format("%s-%d-g%s",
+                          this.nextTag.getName(),
+                          this.distance,
+                          this.abbreviatedCommitId);
+            } else {
+              return "undefined";
+            }
+        }
