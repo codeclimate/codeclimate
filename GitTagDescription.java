0ed0279f94ff0f524d20dd5878f47eb1d2105c6c
Emily Fueger <efueger@users.noreply.github.com>
2022-05-27 09:15:40 -0400

    remove

--- GitTagDescription.java
+++ GitTagDescription.java
@@ -17,20 +17,4 @@
             } else {
               return "undefined";
             }
-            
-   public String toString() { 
-        if (this.nextTag == null) {
-            return this.abbreviatedCommitId;
-        } else if (this.distance == 0) {
-            return this.nextTag.getName();
-        } else {
-            if (!this.nextTag.getName().isEmpty() &&
-                this.distance != 0) {
-                      return String.format("%s-%d-g%s",
-                          this.nextTag.getName(),
-                          this.distance,
-                          this.abbreviatedCommitId);
-            } else {
-              return "undefined";
-            }
         }
