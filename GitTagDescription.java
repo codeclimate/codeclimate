5ae279894f414b02a6ce62e8eacaff0e3408419b
Emily Fueger <efueger@users.noreply.github.com>
2022-05-27 09:14:09 -0400

    add duplicate

--- GitTagDescription.java
+++ GitTagDescription.java
@@ -17,4 +17,20 @@
             } else {
               return "undefined";
             }
+            
+   public String toString() { 
+        if (this.nextTag == null) {
+            return this.abbreviatedCommitId;
+        } else if (this.distance == 0) {
+            return this.nextTag.getName();
+        } else {
+            if (!this.nextTag.getName().isEmpty() &&
+                this.distance != 0) {
+                      return String.format("%s-%d-g%s",
+                          this.nextTag.getName(),
+                          this.distance,
+                          this.abbreviatedCommitId);
+            } else {
+              return "undefined";
+            }
         }
